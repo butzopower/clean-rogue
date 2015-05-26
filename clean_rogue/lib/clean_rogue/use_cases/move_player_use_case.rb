355ac232c0bf7b8cce5211c7b7e77e90e1bc09a7
Brian Butz <butzopower@gmail.com>
2015-05-25 21:18:14 -0700

    Refactor

--- clean_rogue/lib/clean_rogue/use_cases/move_player_use_case.rb
+++ clean_rogue/lib/clean_rogue/use_cases/move_player_use_case.rb
@@ -13,10 +13,7 @@ module CleanRogue
         new_position_y = @player.position[1] + @direction[1]
 
         if valid_move?(new_position_x, new_position_y)
-          new_position = [new_position_x, new_position_y]
-          updated_player = Values::Player.new(position: new_position)
-          updated_room = Values::Room.new(@room.attributes.merge(player: updated_player))
-          @observer.room_updated(updated_room)
+          move_player_to(new_position_x, new_position_y)
         else
           @observer.action_failed("Cannot move outside of room")
         end
@@ -27,6 +24,13 @@ module CleanRogue
       def valid_move?(new_position_x, new_position_y)
         new_position_x >= 0 && new_position_x < @room.width &&
             new_position_y >= 0 && new_position_y < @room.height
+      end
+
+      def move_player_to(new_position_x, new_position_y)
+        new_position = [new_position_x, new_position_y]
+        updated_player = Values::Player.new(position: new_position)
+        updated_room = Values::Room.new(@room.attributes.merge(player: updated_player))
+        @observer.room_updated(updated_room)
       end
     end
   end
