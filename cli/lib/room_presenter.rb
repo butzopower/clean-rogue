2b300871b4d4b8050fe0f9ea8accf38b49a65678
Brian Butz <bbutz@pivotal.io>
2020-09-13 20:50:11 +0100

    Refactor room presenter to use tile objects with rules

--- cli/lib/room_presenter.rb
+++ cli/lib/room_presenter.rb
@@ -1,25 +1,16 @@
 class RoomPresenter
   def initialize(background: '.')
-    @background = background
+    @layers = build_layers(background)
   end
 
   def present_room(room, vision)
-    player = room.player
     height = room.height
     width = room.width
 
-    tiles =  build_positions(height, width).map do |position|
-      if !vision.visible?(position)
-        " "
-      elsif player.position == position
-        "@"
-      elsif room.obstacles.any? { |obstacle| obstacle.position == position}
-        "#"
-      elsif room.items.any? { |item| item.position == position}
-        "o"
-      else
-        @background
-      end
+    tiles = build_positions(height, width).map do |position|
+      @layers
+        .find {|l| l.satisfies?(position: position, room: room, vision: vision) }
+        .display
     end
 
     tiles.each_slice(width)
@@ -34,6 +25,48 @@ class RoomPresenter
       (0...width).to_a.map.with_index do |column_index|
         [column_index, row_index]
       end
+    end
+  end
+
+  def build_layers(background)
+    [
+      UnableToSeeTile.new(' '),
+      PlayerTile.new('@'),
+      ObstacleTile.new('#'),
+      ItemTitle.new('o'),
+      DefaultTile.new(background)
+    ]
+  end
+
+  Tile = Struct.new(:display)
+
+  class UnableToSeeTile < Tile
+    def satisfies?(position:, vision:, **_)
+      !vision.visible?(position)
+    end
+  end
+
+  class PlayerTile < Tile
+    def satisfies?(position:, room:, **_)
+      room.player.position == position
+    end
+  end
+
+  class ObstacleTile < Tile
+    def satisfies?(position:, room:, **_)
+      room.obstacles.any? { |obstacle| obstacle.position == position}
+    end
+  end
+
+  class ItemTitle < Tile
+    def satisfies?(position:, room:, **_)
+      room.items.any? { |item| item.position == position}
+    end
+  end
+
+  class DefaultTile < Tile
+    def satisfies?(**_)
+      true
     end
   end
 end
